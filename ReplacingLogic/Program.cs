69706858a5a19fdd5c98c13734997e07a32542ed
vfrfv <valuev.12@mail.ru>
2024-03-31 17:14:59 +0300

    init2

---
+++ ReplacingLogic/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IMJunior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentSystemsFactory paymentSystemsFactory = new PaymentSystemsFactory();
+            List<IPaymentSystem> paymentSystems = paymentSystemsFactory.CreateAllPaymentSystems();
+
+            Shop shop = new Shop(paymentSystems);
+            shop.Run();
+        }
+    }
+
+    class Shop
+    {
+        private List<IPaymentSystem> _paymentSystems;
+
+        public Shop(List<IPaymentSystem> paymentSystems)
+        {
+            _paymentSystems = paymentSystems ?? throw new ArgumentNullException(nameof(paymentSystems));
+        }
+
+        public void Run()
+        {
+            var orderForm = new OrderForm(_paymentSystems.Select(system => system.Name).ToList());
+            var systemId = orderForm.ShowForm();
+
+            foreach (var paymentSistem in _paymentSystems)
+            {
+                if (paymentSistem.Name.Equals(systemId, StringComparison.OrdinalIgnoreCase))
+                {
+                    paymentSistem.AcceptPayment();
+                }
+            }
+        }
+    }
+
+    public class OrderForm
+    {
+        public OrderForm(List<string> supportedPaymentSystems)
+        {
+            SupportedPaymentSystems = supportedPaymentSystems ?? throw new ArgumentNullException(nameof(supportedPaymentSystems));
+        }
+
+        public IReadOnlyList<string> SupportedPaymentSystems { get; }
+
+        public string ShowForm()
+        {
+            Console.WriteLine($"Мы принимаем: {string.Join(", ", SupportedPaymentSystems)}");
+            Console.WriteLine("Какое системой вы хотите совершить оплату?");
+
+            return Console.ReadLine();
+        }
+    }
+
+    class PaymentSystemsFactory
+    {
+        public IPaymentSystem CreateQiwi()
+        {
+            return new Qiwi();
+        }
+
+        public IPaymentSystem CreateWebMoney()
+        {
+            return new WebMoney();
+        }
+
+        public IPaymentSystem CreateCard()
+        {
+            return new Card();
+        }
+
+        public List<IPaymentSystem> CreateAllPaymentSystems()
+        {
+            return new List<IPaymentSystem>()
+            {
+                CreateQiwi(),
+                CreateWebMoney(),
+                CreateCard()
+            };
+        }
+    }
+
+    interface IPaymentSystem
+    {
+        string Name { get; }
+
+        void AcceptPayment();
+    }
+
+    class Qiwi : IPaymentSystem
+    {
+        public Qiwi()
+        {
+            Name = "QIWI";
+        }
+
+        public string Name { get; private set; }
+
+        public void AcceptPayment()
+        {
+            Console.WriteLine("Перевод на страницу QIWI...");
+            Console.WriteLine("Проверка платежа через QIWI...");
+            Console.WriteLine("Оплата прошла успешно!");
+        }
+    }
+
+    class WebMoney : IPaymentSystem
+    {
+        public WebMoney()
+        {
+            Name = "WebMoney";
+        }
+
+        public string Name { get; private set; }
+
+        public void AcceptPayment()
+        {
+            Console.WriteLine("Вызов API WebMoney...");
+            Console.WriteLine("Проверка платежа через WebMoney...");
+            Console.WriteLine("Оплата прошла успешно!");
+        }
+    }
+
+    class Card : IPaymentSystem
+    {
+        public Card()
+        {
+            Name = "Card";
+        }
+
+        public string Name { get; private set; }
+
+        public void AcceptPayment()
+        {
+            Console.WriteLine("Вызов API банка эмитера карты Card...");
+            Console.WriteLine("Проверка платежа через Card...");
+            Console.WriteLine("Оплата прошла успешно!");
+        }
+    }
+}
---
+++ ReplacingLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ReplacingLogic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReplacingLogic")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2bc6bbb6-1e32-40b3-8f8b-d8e6fc23d814")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
